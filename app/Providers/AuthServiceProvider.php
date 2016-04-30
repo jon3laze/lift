870a668064fb5a1e53117930e6a1229ccd906a85
Jon Ouellette <jonaouellette@gmail.com>
2016-04-30 04:26:15 -0700

    Add permission and roles
    Working

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Permission;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,8 +25,17 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(GateContract $gate)
     {
-        $this->registerPolicies($gate);
+        parent::registerPolicies($gate);
 
-        //
+        foreach($this->getPermissions() as $permission) {
+            $gate->define($permission->name, function($user) use ($permission) {
+                return $user->hasRole($permission->roles);
+            });
+        }
+    }
+
+    protected function getPermissions()
+    {
+        return Permission::with('roles')->get();
     }
 }
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -13,12 +13,30 @@ lift | settings
 			  <li role="presentation"><a href="#">Roles</a></li>
 			  <li role="presentation"><a href="#">Permissions</a></li>
 			</ul>
-            <div class="well">
-            	<ul>
-            	@foreach($users as $user)
-            		<li> {{ $user->name }} </li>
-            	@endforeach
-            	</ul>
+            <div class="well center-block">
+                <div class="table-responsive center-block">
+                    @foreach($users as $user)
+                        <span>
+                            <i class="fa fa-user fa-fw"></i>
+                            {{ $user->name }}
+                            @can('user_edit')
+                                    <a class="btn btn-default" href="#">
+                                        <i class="fa fa-pencil" title="Edit" aria-hidden="true"></i>
+                                        <span class="sr-only">Edit</span>
+                                    </a>
+                            @endcan
+                        </span>
+                    @endforeach
+                    <hr>
+                    @can('user_create')
+                        <span>
+                            <a class="btn btn-default" href="#">
+                                <i class="fa fa-plus" title="Add User" aria-hidden="true"></i>
+                                <span class="sr-only">Add User</span>
+                            </a>
+                        </span>
+                    @endcan
+                </div>
             </div>
         </div>
     </div>
