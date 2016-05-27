3125562fe459034ffb179f7325a5c459b91d9ad6
Jon Ouellette <jonaouellette@gmail.com>
2016-05-26 17:02:55 -0700

    fix form display

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -23,7 +23,7 @@ lift | profile
     </div>
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="col-md-5 col-md-offset-3">
+            <div class="col-md-4 col-md-offset-4">
                 {!! Form::model($user, array('route' => array('profile.update', $user->id), 'files' => true, 'class' => 'form-horizontal')) !!}
                 {!! method_field('PATCH') !!}
                 <table class="table table-condensed table-hover">
